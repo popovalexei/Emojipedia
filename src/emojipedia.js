ac23bb827baa1fc5b81eea75367f2fa964fc19a1
Alexei Popov <106864523+popovalexei@users.noreply.github.com>
2023-03-09 11:33:54 +0200

    Add initial files

---
+++ src/emojipedia.js
@@ -0,0 +1,67 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "🤓",
+    name: "Nerd Face",
+    meaning:
+      "Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits."
+  },
+  {
+    id: 5,
+    emoji: "😎",
+    name: "Face With Sunglasses",
+    meaning:
+      "I'm the king of the world! Mr. Cool Smiley with sunglasses. Is totally self-assured and relaxed. Everything is absolutely easy and cool. Also a symbol for sun, summer holidays, and holidays on the beach."
+  },
+  {
+    id: 6,
+    emoji: "🌲",
+    name: "Evergreen Tree",
+    meaning:
+      "Evergreen coniferous trees do not lose their needles even in winter. In China and Japan bonsai is a form of artistic garden design. Symbol of strength and immortality."
+  },
+  {
+    id: 7,
+    emoji: "👨‍🎓",
+    name: "Male student",
+    meaning:
+      "A brand new university graduate! At some colleges, cap and gown or doctoral cap are worn at graduation ceremonies following the American example. Symbol for (further) education."
+  },
+  {
+    id: 8,
+    emoji: "🤪",
+    name: "Crazy Face",
+    meaning:
+      "Something is insanely funny. The mood is exuberant - you are totally silly and crazy. Totally freaking out with enthusiasm or joy. Maybe someone has even told an indecent joke."
+  },
+  {
+    id: 9,
+    emoji: "🚕",
+    name: "Taxi",
+    meaning:
+      "We’ll call a taxi! A taxi is a means of public transportation for occasional passenger transportation. Taxis are available worldwide, most of them in major cities. The emoji can indicate a fast and comfortable transport."
+  }
+];
+
+export default emojipedia;
